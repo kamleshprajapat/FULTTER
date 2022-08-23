f1586189b0b4b7b2323c080396d8c1def3f05725
kamleshprajapat <78674283+kamleshprajapat@users.noreply.github.com>
2022-08-23 14:44:25 +0530

    ok
    ok

---
+++ TASK/classtask.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() {
+  var num = 0;
+  var name;
+
+  print("enter your name");
+  name = stdin.readLineSync()!;
+  for (var i = 0; i < name.length; i++)
+   {
+    num = num + 1;
+
+    print("$num ${name[i]}");
+  }
+  print("There are in $num cartory");
+}
